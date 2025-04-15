419541b1efa2848226bee3930de023f849bf739c
nisar <nisargakabballi38@gmail.com>
2025-04-15 19:33:00 +0530

    adding files

---
+++ abstractclass/src/com/xworkz/internal/ElectricScooter.java
@@ -0,0 +1,10 @@
+package com.xworkz.internal;
+
+    public class ElectricScooter extends Transport {
+
+        @Override
+        void move() {
+            System.out.println("Electric scooter is moving.");
+        }
+    }
+
