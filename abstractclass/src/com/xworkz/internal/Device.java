02813a984c1ce3983c6e01e8710c647caf0972a8
nisar <nisargakabballi38@gmail.com>
2025-04-15 19:24:48 +0530

    adding files

---
+++ abstractclass/src/com/xworkz/internal/Device.java
@@ -0,0 +1,8 @@
+package com.xworkz.internal;
+
+    public abstract class Device {
+        public void display() {
+            System.out.println("This is an abstract class with no abstract methods.");
+        }
+    }
+
