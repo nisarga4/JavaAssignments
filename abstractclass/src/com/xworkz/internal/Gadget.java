5e04550c9f2d28f85211d37d4851f3aa7adbff6d
nisar <nisargakabballi38@gmail.com>
2025-04-15 19:33:15 +0530

    adding files

---
+++ abstractclass/src/com/xworkz/internal/Gadget.java
@@ -0,0 +1,8 @@
+package com.xworkz.internal;
+    public abstract class Gadget {
+        abstract void operate();
+
+        public void charge() {
+            System.out.println("Charging the gadget.");
+        }
+    }
